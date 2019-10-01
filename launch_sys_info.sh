8adea34f00e395b07e98735ad008e86626dfa119
Haotian Zhang <htplex@gmail.com>
2019-10-01 19:54:37 -0400

    update start script

--- launch_sys_info.sh
+++ launch_sys_info.sh
@@ -1,1 +1,3 @@
-python get_machine_status/src/post_info.py &
+tmux new -s sys_info
+tmux send-keys -t sys_info 'conda activate sys_info' C-m
+tmux send-keys -t sys_info 'python get_machine_status/src/post_info.py' C-m
