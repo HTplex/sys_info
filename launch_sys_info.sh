53a728ad1ebc83db765e75ec3846a3143dff8932
agent_h <htplex@gmail.com>
2019-10-02 09:47:11 -0400

    tmux integrate

--- launch_sys_info.sh
+++ launch_sys_info.sh
@@ -1,3 +1,7 @@
-tmux new -s sys_info
-tmux send-keys -t sys_info 'conda activate sys_info' C-m
-tmux send-keys -t sys_info 'python get_machine_status/src/post_info.py' C-m
+#!/bin/bash
+
+tmux new-session -d -s sys_info 
+tmux send-keys -t sys_info:0 'conda activate sys_info' C-m
+sleep 1
+tmux send-keys -t sys_info:0 'python get_machine_status/src/post_info.py' C-m
+
